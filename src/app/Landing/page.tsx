348a71d3ac7bcf5c5a5ce703c372d16ed1968f83
Nwakodo Ifeanyi <46559372+ify12345@users.noreply.github.com>
2024-07-23 10:26:31 +0100

    landing-page done

--- src/app/Landing/page.tsx
+++ src/app/Landing/page.tsx
@@ -1,27 +1,40 @@
 "use client";
-import NavBar from "@/components/NavBar";
-import Hero from "@/components/Hero";
-import Advantage from "@/components/Advantage";
-import Earning from "@/components/Earning";
-import Innovative from "@/components/Innovative";
-import Faq from "@/components/Faq";
-import GetApp from "@/components/GetApp";
-import Footer from "@/components/Footer";
-import Cookie from "@/components/Cookie";
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+
+const NavBar = dynamic(() => import('@/components/NavBar'), { ssr: false });
+const Hero = dynamic(() => import('@/components/Hero'), { ssr: false });
+const Advantage = dynamic(() => import('@/components/Advantage'), { ssr: false });
+const Earning = dynamic(() => import('@/components/Earning'), { ssr: false });
+const Innovative = dynamic(() => import('@/components/Innovative'), { ssr: false });
+const Faq = dynamic(() => import('@/components/Faq'), { ssr: false });
+const GetApp = dynamic(() => import('@/components/GetApp'), { ssr: false });
+const Footer = dynamic(() => import('@/components/Footer'), { ssr: false });
+const Cookie = dynamic(() => import('@/components/Cookie'), { ssr: false });
 
 export default function Landing() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; 
+  }
+
   return (
     <div className="flex flex-col">
       <NavBar />
       <div className="mt-[9rem]">
-      <Hero/>
-      <Advantage/>
-      <Earning/>
-      <Innovative/>
-      <Faq/>
-      <GetApp/>
-      <Footer/>
-      <Cookie/>
+        <Hero />
+        <Advantage />
+        <Earning />
+        <Innovative />
+        <Faq />
+        <GetApp />
+        <Footer />
+        <Cookie />
       </div>
     </div>
   );
