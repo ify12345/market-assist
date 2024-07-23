916e372b5884fb0998cf44fb50d41d5be9c66ea7
Nwakodo Ifeanyi <46559372+ify12345@users.noreply.github.com>
2024-07-23 10:17:47 +0100

    landing-page done

--- src/app/Landing/page.tsx
+++ src/app/Landing/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import React, { useState } from "react";
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import Advantage from "@/components/Advantage";
@@ -11,17 +10,6 @@ import Footer from "@/components/Footer";
 import Cookie from "@/components/Cookie";
 
 export default function Landing() {
-  const [loading, setLoading] = useState(false);
-
-  const handleClick = () => {
-    setLoading(true);
-    // Simulate an async operation
-    setTimeout(() => {
-      setLoading(false);
-      console.log("Button clicked!");
-    }, 2000);
-  };
-
   return (
     <div className="flex flex-col">
       <NavBar />
